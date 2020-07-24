6b693263391abe38e70768b87d0c4084350e14f1
EC2 Default User <ec2-user@ip-172-31-40-139.ec2.internal>
2020-07-24 15:39:22 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
-	  Hi there - changed!
+	  Hi there - changed on Feature branch!
         </p>
         <a
           className="App-link"
